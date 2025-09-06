b43a2f09939cf359a434a382bcad85fc8e705336
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-06 08:24:22 +0100

    Navbar design contd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/images/logo.png'
+import { IoSearch } from "react-icons/io5";
 import Button from '../Button/Button'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-
+      
         <div className='navLogo'>
             <img src={logo} alt="" />
             <h3>tlas Rentals</h3>
@@ -15,16 +16,16 @@ const Navbar = () => {
 
         <div className='navLinks'>
             <a href="">Home</a>
-            <a href="">Features</a>
-            <a href="">Testimonials</a>
-            <a href="">Pricing</a>
-            <a href="">FAQs</a>
+            <a href="">Discover <IoSearch className='navSearch'/></a>
+            <a href="">Properties</a>
+            <a href="">Hosts</a>
         </div>
 
         <div className='navBtn'>
             
             <button className='navBtnBtn'>Log In</button>
-            <Button width="150px" height="38px" title="Sign Up"/>
+            <h4>Become a host</h4>
+            {/* <Button width="150px" height="38px" title="Sign Up"/> */}
         </div>
 
     </div>
